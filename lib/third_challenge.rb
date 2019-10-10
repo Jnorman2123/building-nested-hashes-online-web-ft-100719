faec8aaca41ebb009d78ef4d8b94bd87ffd46fea
Jonathan Norman <jnorman2123@gmail.com>
2019-10-10 14:09:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,13 +4,19 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
+      :hero_friends => [
+        first_friend: {name: "Benvolio", age: "17", attitude: "worried"},
+        second_friend: {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => []
+      :heroine_friends => [
+        first_friend: {name: "Steven", age: "30", attitude: "confused"},
+        second_friend: {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
   }
 
